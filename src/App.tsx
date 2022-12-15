e56484ac28f9df4a6421b1b9b41fc30fadf1b122
FebeFarina <alu0101430973@ull.edu.es>
2022-12-15 23:07:24 +0000

    feat: title, description and input

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,23 @@
-import { useState } from 'react';
-import './App.css';
-import reactLogo from './assets/react.svg';
+import { useState } from 'react'
+import './App.scss';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="back_button">
+          </div>
+        <text className="email_title">Email
+        </text>
+        <text className="email_description">
+        Lorem ipsum dolor sit amet, 
+        consectetur adipiscing elit. 
+        Maecenas lacus purus, hendrerit eu libero sit amet.
+        </text>
+        <div className="email_input"> 
+          <input className ="rectangle" placeholder="Email" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
   );
 }
