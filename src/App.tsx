6d3337debe671d4c65c71eaaed71993e6defcaa4
Alegarpi <alegarp14@gmail.com>
2022-12-20 20:24:33 +0000

    Modificado App.tsx para prueba de MenuLink

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import LoginPasswordRecovery from "./pages/LoginPasswordRecovery";
 import LoginScreen from "./pages/LoginScreen";
@@ -46,6 +47,35 @@ function App() {
             </Routes>
         </Router>
     );
+=======
+import { useState } from 'react';
+import './App.scss';
+import reactLogo from './assets/react.svg';
+import notification from './assets/notification.svg'
+import MenuLink from './components/MenuLink';
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <MenuLink img={notification} link="#" linkName="Notificacion"/>
+        <MenuLink img={reactLogo} link="https://reactjs.org" linkName="Link de REact"/>
+      </div>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    </div>
+  );
+>>>>>>> 7c8844f (Modificado App.tsx para prueba de MenuLink)
 }
 
 export default App;
