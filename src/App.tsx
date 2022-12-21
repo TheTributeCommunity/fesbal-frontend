5591e48f270c6bf761b8df2146d2af2d5fd8af8e
DiegoPerezGarcia <alu0101345918@ull.edu.es>
2022-12-21 23:11:22 +0000

    Update: App.tsx add Bottom Images and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
-import { useState } from 'react';
 import './App.scss';
-import reactLogo from './assets/react.svg';
+import ImageLink from './components/recipients-landing-screen/Icons_below';
+import clock from './assets/clock.svg';
+import location from './assets/location.svg';
+import notification from './assets/notification.svg';
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="Bottom-Images">
+        <ImageLink href_link='' src_img={clock} alt='clock image'/>
+        <ImageLink href_link='' src_img={location} alt='location image'/>
+        <ImageLink href_link='' src_img={notification} alt='ring image'/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
   );
 }
