d995592e4da79bda98357ce493322e66adfbe97c
Dani García <danielgarciaher2@gmail.com>
2022-12-21 21:57:58 +0000

    css update

--- src/NewFamilyMember.tsx
+++ src/NewFamilyMember.tsx
@@ -5,24 +5,53 @@ function handleChangeName(name: string) {
   const  emailValid = name ? true : false;
 }
 
+// function ControlButton(object: props) {
+//   const nameButton = document.getElementById('checkout-button');
+//   const surnameButton = document.getElementById('checkout-button');
+//   const button = document.getElementById('checkout-button');
+//   const button = document.getElementById('checkout-button');
+// }
+enum DocumentType {
+  dni,
+  nie,
+  pasaporte
+};
+
 function NewFamilyMember() {
+  const [name, setName] = useState<string>('');
+  const [surname, setSurname] = useState<string>('');
+  const [date, setDate] = useState<string>('');
+  const [documentType, setDocumentType] = useState<DocumentType>();
+  const [documentText, setDocumentText] = useState<string>('');
+
+  let disabled: boolean;
+  if ((name !== '') && (surname !== '') && (date !== '') && (documentText !== '') &&
+      (documentType !== undefined)) {
+    disabled = false;
+  } else {
+    disabled = true;
+  }
+  
   return (
     <div className="NewFamilyMember">
       <form className="Form">
-        <h2 className="Title">Añadir familiar</h2>
-        <input type="text" className="Input" placeholder="Nombre" />
-        <input type="text" className="Input" placeholder="Apellidos" />
-        <input type="date" className="Input" placeholder="Fecha de nacimiento" />
+        <div className="Header">
+          <h2 className="Title">Añadir familiar</h2>
+          <button className="back-button">x</button>
+        </div>
+        <input type="text" id="Name" className="Input" placeholder="Nombre" />
+        <input type="text" id="Surname" className="Input" placeholder="Apellidos" />
+        <input type="date" id="BirthDate" className="Input" placeholder="Fecha de nacimiento" />
         <div className="DocumentType">
-          <select className="Input">
+          <select id="DocumentType" className="Input">
             <option value="Tipo" disabled selected hidden>Tipo</option>
             <option value="dni">DNI</option>
             <option value="nie">NIE</option>
             <option value="pasaporte">Pasaporte</option>
           </select>
-          <input type="text" className="Input" placeholder="Número de documento" />
+          <input type="DocumentText" className="Input" placeholder="Número de documento" />
         </div>
-        <button className="AddRelative">Añadir familiar</button>
+        <button className="AddRelative" disabled={disabled}>Añadir familiar</button>
       </form>
     </div>
   );
