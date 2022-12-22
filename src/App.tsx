34c194e827f6145c3d87559bfd7358c9a2a127da
BorjaZarco <borjazarco@theagilemonkeys.com>
2022-12-22 13:10:44 +0000

    install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react';
-import './App.scss';
 import reactLogo from './assets/react.svg';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div className="bg-red-600">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
