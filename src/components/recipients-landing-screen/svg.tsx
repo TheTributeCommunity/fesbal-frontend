3f85cdb29cdc81e4d5ec7a34206e60d86cc7c28b
Abian <alu0101487458@ull.edu.es>
2022-12-28 03:17:38 +0000

    Add: New react component to SVG image

---
+++ src/components/recipients-landing-screen/svg.tsx
@@ -0,0 +1,12 @@
+import ClockImage from '../../assets/clock.svg'
+import NotificationImage from '../../assets/notification.svg'
+import LocationImage from '../../assets/location.svg'
+
+const ClockIcon = () => {
+  return (
+  <svg className="image-icon">
+    <img src={ClockImage} alt="clock image" />
+  </svg>)
+}
+
+export default ClockIcon
