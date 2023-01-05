25b9c645a8e2c1d8b7f6a76f449b2f8c86ac990b
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-01-05 22:45:02 +0000

    fix bug causing inputs to shift downward when label appears

--- src/components/molecules/IdentificationForm.tsx
+++ src/components/molecules/IdentificationForm.tsx
@@ -21,19 +21,17 @@ const IdentificationForm = () => {
               className="flex flex-col gap-4 h-full mt-8 self-center md:w-1/2 lg:w-1/3 justify-between">
             <div className="flex flex-row gap-4">
                 <div className="flex flex-col gap-1.5 text-sm text-blue-light">
-                    {selectedOption &&
-                        <label htmlFor="identityType">Tipo</label>}
+                    <label htmlFor="identityType" className={selectedOption ? "opacity-100" : "opacity-0"}>Tipo</label>
                     <IdentificationSelect options={selectOptions} value={selectedOption}
                                           onChange={handleSelectChange}/>
                 </div>
                 <div className="flex flex-col gap-1.5 text-sm text-blue-light w-full">
-                    {inputValue &&
-                        <label htmlFor="identityNumber">Número de
-                            documento</label>}
+                    <label htmlFor="identityNumber" className={inputValue ? "opacity-100" : "opacity-0"}>Número de
+                        identidad</label>
                     <IdentificationInput value={inputValue} onChange={handleInputChange}/>
                 </div>
             </div>
-            <AppContinueButton disabled={!validateInput()} />
+            <AppContinueButton disabled={!validateInput()}/>
         </form>
     )
 }
