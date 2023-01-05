b0b0ebecb3d15276b4d2064d7be9c897d4187fc7
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-01-05 02:57:50 +0000

    add Select for DNI/NIE

---
+++ src/components/molecules/IdentificationSelect.tsx
@@ -0,0 +1,42 @@
+import React, {FC, useState} from 'react';
+import {IdentificationSelectOption, IdentificationSelectProps} from "../../types/IdentificationSelectProps";
+import IdentificationSelected from "../atom/IdentificationSelected";
+
+
+const IdentificationSelect: FC<IdentificationSelectProps> = ({options, value, onChange}) => {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState(value);
+    const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
+    const closeDropdown = () => setIsDropdownOpen(false);
+    const handleOptionClick = (option: IdentificationSelectOption) => {
+        setSelectedOption(option);
+        onChange(option);
+        closeDropdown();
+    }
+
+    return (
+        <div className="relative rounded-md text-blue-light bg-white">
+            <div onClick={toggleDropdown}>
+                <IdentificationSelected value={selectedOption}/>
+            </div>
+            {isDropdownOpen && (
+                <div className="origin-top-left absolute left-0 z-10 mt-1 w-full rounded-md">
+                    <ul className="py-1 bg-white rounded-md">
+                        {options.map((option) => (
+                            <li
+                                key={option}
+                                className="block px-4 py-2 text-sm leading-5 text-blue-light hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
+                                role="menuitem"
+                                onClick={() => handleOptionClick(option)}
+                            >
+                                {option}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default IdentificationSelect
