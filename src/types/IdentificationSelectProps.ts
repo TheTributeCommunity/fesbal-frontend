c733f24ce8af64ba0a7199b2479670921dc9487e
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-01-05 02:52:24 +0000

    add interface for DNI/NIE Select

---
+++ src/types/IdentificationSelectProps.ts
@@ -0,0 +1,9 @@
+export interface IdentificationSelectProps {
+    options: IdentificationSelectOption[];
+    value: IdentificationSelectOption
+    onChange: (value: IdentificationSelectOption) => void;
+}
+
+export type IdentificationSelectOption = "DNI" | "NIE" | undefined;
+
+
