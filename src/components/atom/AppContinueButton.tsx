b8e45b0df7bb36976928fb36a9afdfe440e3e3f1
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-01-05 22:30:24 +0000

    remove unnecesary onClick on continue button

--- src/components/atom/AppContinueButton.tsx
+++ src/components/atom/AppContinueButton.tsx
@@ -1,12 +1,11 @@
 import {FC} from "react";
-import AppContinueButtonProps from "../../types/AppContinueButton";
 
-const AppContinueButton: FC<AppContinueButtonProps> = ({disabled, onClick}) => {
+const AppContinueButton = ({disabled}: {disabled: boolean}) => {
     return (
         <button
             className="rounded-2xl flex items-center justify-center py-5 text-white font-bold bg-blue-light
                 disabled:bg-white disabled:text-[#0F95CE] border border-[#0F95CE] disabled:opacity-50"
-            disabled={disabled} onClick={onClick}>
+            disabled={disabled} type="submit">
             Continuar
         </button>
     );
