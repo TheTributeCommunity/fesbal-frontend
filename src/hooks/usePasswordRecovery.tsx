fa19e5254b8a6abf74c1f6ffd76cee7352dbe31b
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-01-06 14:05:16 +0000

    install sweetalert2 and create alert for send email on recovery password

--- src/hooks/usePasswordRecovery.tsx
+++ src/hooks/usePasswordRecovery.tsx
@@ -1,6 +1,7 @@
 import {ChangeEvent, FormEvent, useState} from "react";
 import {UserEmail} from "../types/UserLoginProps";
 import usersMock from "../mocks/users.mock";
+import Swal from 'sweetalert2'
 
 const usePasswordRecovery = <T extends UserEmail>(initialState: T) => {
     const [email, setEmail] = useState<T>(initialState);
@@ -15,7 +16,21 @@ const usePasswordRecovery = <T extends UserEmail>(initialState: T) => {
     const onSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         setHasError(!isFormValid(email));
-        if (isFormValid(email)) alert("Email enviado");
+        if (isFormValid(email)) Swal.fire({
+            title: 'Email enviado',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacus purus.',
+
+            icon: 'success',
+            confirmButtonText: 'Ir a Iniciar Sesión',
+            color: '#002E5D',
+            iconColor: '#0F95CE',
+            confirmButtonColor: '#0F95CE',
+            position: 'bottom',
+            padding: '1rem',
+            customClass: { popup: "swal2-border-radius", htmlContainer: "swal2-text-left",
+                confirmButton: "swal2-confirm-button", actions:"swal2-actions", title: "swal2-title" },
+            width: parent.innerWidth < 768 ? '95%' : '50%',
+        })
     }
 
     return {
