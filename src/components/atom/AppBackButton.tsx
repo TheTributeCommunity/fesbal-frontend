7e50505df3de48e909f6deaf71e949c2612623e8
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-01-06 12:55:56 +0000

    add route for AppBackButton

--- src/components/atom/AppBackButton.tsx
+++ src/components/atom/AppBackButton.tsx
@@ -1,11 +1,12 @@
 import LeftArrowIcon from "../icons/LeftArrow";
+import {NavLink} from "react-router-dom";
 
-const AppBackButton = () => {
+const AppBackButton = ({goTo}: { goTo: string }) => {
     return (
-        <button
+        <NavLink to={goTo}
             className="bg-white h-12 w-12 rounded-full flex items-center justify-center app-shadow">
             <LeftArrowIcon/>
-        </button>
+        </NavLink>
     );
 };
 
