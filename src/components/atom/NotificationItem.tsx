6c61f4d08d7f37f2c3e784253a0b7269b712cbaf
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-01-06 20:34:52 +0000

    add notification item

---
+++ src/components/atom/NotificationItem.tsx
@@ -0,0 +1,18 @@
+import NotificationProps from "../../types/NotificationProps";
+import {FC} from "react";
+import {NavLink} from "react-router-dom";
+
+const NotificationItem: FC<NotificationProps> = ({id, title, message, date, hasBeenRead}) => {
+    return (
+        <>
+            <NavLink className={`${hasBeenRead ? "bg-[#F2FBFF]" : "bg-white"} rounded-xl py-5 px-8`}
+                     to={`/notifications/${id}`}>
+                <p className="text-blue-light text-sm">{date}</p>
+                <h2 className="text-l font-bold">{title}</h2>
+                <p className="text-blue-dark">{message.length > 80 ? message.substring(0, 80) + "..." : message}</p>
+            </NavLink>
+        </>
+    );
+};
+
+export default NotificationItem;
