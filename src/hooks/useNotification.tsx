02b3d71ca5c443bbfe40ef09c2647006630bf3a7
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-01-06 20:34:36 +0000

    add custom hook for notification(set notification as readed)

---
+++ src/hooks/useNotification.tsx
@@ -0,0 +1,20 @@
+import {useEffect} from "react";
+import NotificationProps from "../types/NotificationProps";
+import notificationsMock from "../mocks/notifications.mock";
+
+const useNotification = (id: string) => {
+    const notification: NotificationProps | undefined = notificationsMock.find((notification) => notification.id === id);
+    const {title, message, date} = notification || {};
+
+    useEffect(() => {
+        notificationsMock.map((notification) => {
+            if (notification.id === id) {
+                notification.hasBeenRead = true;
+            }
+        });
+    });
+
+    return {title, message, date};
+};
+
+export default useNotification;
