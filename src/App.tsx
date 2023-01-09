f2c2c5b13859bba7b0f715800045f1a18a034225
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-01-09 15:18:27 +0000

    add new pages to the router

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import PasswordRecovery from "./pages/PasswordRecovery";
 import PickupPoint from "./pages/PickupPoint";
 import NotificationsScreen from "./pages/NotificationsScreen";
 import NotificationDetails from "./pages/NotificationDetails";
+import EditEmail from "./pages/EditEmail";
+import EditPrevPassword from "./pages/EditPrevPassword";
+import ProfileScreen from "./pages/ProfileScreen";
+import EditNewPassword from "./pages/EditNewPassword";
 
 function App() {
     return (
@@ -14,6 +18,10 @@ function App() {
                 <Route path="/pickup-point" element={<PickupPoint/>}/>
                 <Route path="/notifications" element={<NotificationsScreen/>}/>
                 <Route path="/notifications/:id" element={<NotificationDetails/>}/>
+                <Route path="/profile/*" element={<ProfileScreen/>}/>
+                <Route path="/profile/edit-email" element={<EditEmail/>}/>
+                <Route path="/profile/edit-prev-password" element={<EditPrevPassword/>}/>
+                <Route path="/profile/edit-new-password" element={<EditNewPassword/>}/>
                 <Route path="*" element={<LoginScreen/>}/>
             </Routes>
         </Router>
