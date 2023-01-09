eabb44959402a8526ec88937862b7159998d9803
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-01-09 15:06:41 +0000

    update user mock adding all data necesary in ProfileScreen

--- src/mocks/users.mock.ts
+++ src/mocks/users.mock.ts
@@ -1,30 +1,90 @@
-import UserLoginProps from "../types/UserLoginProps";
+import UserProps from "../types/UserProps";
 
-const users: UserLoginProps[] = [
+const users: UserProps[] = [
     {
         id: "12345678A",
         password: "password1",
-        email: "test1@gmail.com"
+        email: "test1@gmail.com",
+        fullName: "Test User 1",
+        birthDate: "01/01/2000",
+        phone: "666666666",
+        familyMembers: [
+            {
+                FullName: "Test User 2",
+                id: "12345678B",
+
+            },
+            {
+                FullName: "Test User 3",
+                id: "X2345678X",
+
+            }
+        ]
+
     },
     {
         id: "12345678B",
         password: "password2",
-        email: "test2@gmail.com"
+        email: "test2@gmail.com",
+        fullName: "Test User 2",
+        birthDate: "01/01/2000",
+        phone: "666666666",
+        familyMembers: [
+            {
+                FullName: "Test User 1",
+                id: "12345678A",
+            },
+            {
+                FullName: "Test User 3",
+                id: "X2345678X",
+            },
+        ]
     },
     {
         id: "X2345678X",
         password: "password3",
-        email: "test3@gmail.com"
+        email: "test3@gmail.com",
+        fullName: "Test User 3",
+        birthDate: "01/01/2000",
+        phone: "666666666",
+        familyMembers: [
+            {
+                FullName: "Test User 1",
+                id: "12345678A",
+            },
+            {
+                FullName: "Test User 2",
+                id: "12345678B",
+            }
+        ]
     },
     {
         id: "Z2345678Z",
         password: "password4",
-        email: "test4@gmail.com"
+        email: "test4@gmail.com",
+        fullName: "Test User 4",
+        birthDate: "01/01/2000",
+        phone: "666666666",
+        familyMembers: [
+            {
+                FullName: "Test User 5",
+                id: "Z2345678Z",
+            }
+        ]
     },
     {
         id: "12345678L",
         password: "password5",
-        email: "test5@gmail.com"
+        email: "test5@gmail.com",
+        fullName: "Test User 5",
+        birthDate: "01/01/2000",
+        phone: "666666666",
+        familyMembers: [
+            {
+                FullName: "Test User 4",
+                id: "Z2345678Z",
+            }
+        ]
     }
 ];
 
