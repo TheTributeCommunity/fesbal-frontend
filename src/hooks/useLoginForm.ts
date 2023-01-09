3266d6d787fa26f39639fa620147518ab95060b0
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-01-09 15:08:01 +0000

    add custom hook for showPassword and update useLogin to remove funcionality of show/toggle password

--- src/hooks/useLoginForm.ts
+++ src/hooks/useLoginForm.ts
@@ -1,34 +1,27 @@
 import {ChangeEvent, FormEvent, useState} from "react";
-import UserLoginProps from "../types/UserLoginProps";
+import UserProps from "../types/UserProps";
 import usersMock from "../mocks/users.mock";
 
-const useLoginForm = <T extends UserLoginProps>(initialState: T) => {
+const useLoginForm = <T extends UserProps>(initialState: T) => {
     const [user, setUser] = useState<T>(initialState);
     const [hasError, setHasError] = useState<boolean>(false);
-    const [showPassword, setShowPassword] = useState<boolean>(false);
     const onChange = (e: ChangeEvent<HTMLInputElement>) => {
         setUser({...user, [e.target.name]: e.target.value});
         setHasError(false);
     }
-    const isFormValid = (user: UserLoginProps): boolean => {
-        return usersMock.some((u: UserLoginProps) => u.id === user.id && u.password === user.password);
+    const isFormValid = (user: UserProps): boolean => {
+        return usersMock.some((u: UserProps) => u.id === user.id && u.password === user.password);
     }
     const onSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         setHasError(!isFormValid(user));
     }
-    const toggleShowPassword = () => {
-        setShowPassword(!showPassword);
-    }
-
 
     return {
         onChange,
         user,
         hasError,
         onSubmit,
-        showPassword,
-        toggleShowPassword
     }
 }
 
