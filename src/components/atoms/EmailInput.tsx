083d69e19fa067186fb2c92e04bfea5e224232f2
Sergio Tabares <alu0101124896@ull.edu.es>
2023-01-10 22:11:00 +0000

    Add email input interactive label

--- src/components/atoms/EmailInput.tsx
+++ src/components/atoms/EmailInput.tsx
@@ -1,17 +1,26 @@
-import {ChangeEventHandler, FC} from 'react';
+import {ChangeEventHandler, FC, useState} from 'react';
 
 interface EmailInputProps {
   onChangeUpdate: ChangeEventHandler<HTMLInputElement>
 }
 
 const EmailInput: FC<EmailInputProps> = ({ onChangeUpdate }) => {
+  const [emptyInput, setEmptiness] = useState(true)
+
   return (
     <div className="email_input">
+      {emptyInput ?
+        null :
+        <label className="small_email">
+          Email
+        </label>
+      }
       <input
         className ="rectangle"
         type="email"
         placeholder="Email"
         onChange={event => {
+          setEmptiness(event.target.value == "")
           onChangeUpdate(event)
         }}
       />
