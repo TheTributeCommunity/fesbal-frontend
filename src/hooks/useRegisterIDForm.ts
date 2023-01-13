bde66bd3277b99a7559a042dbecbe4139d4291f9
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-01-13 00:31:45 +0000

    Add to RegisterId the navigation for RegisterReferral

--- src/hooks/useRegisterIDForm.ts
+++ src/hooks/useRegisterIDForm.ts
@@ -20,7 +20,7 @@ const useRegisterIDForm = () => {
     const onSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if (validateUserID()) {
-            navigate('/profile');
+            navigate('/register/referral');
         }
     }
 
