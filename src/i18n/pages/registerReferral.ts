c38bccfaf5614a39e84493072cc02c9a99bc6dec
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-01-13 00:35:39 +0000

    Add jpeg to valid formats on i18n

--- src/i18n/pages/registerReferral.ts
+++ src/i18n/pages/registerReferral.ts
@@ -1,6 +1,6 @@
 export const registerReferralEs = {
     title: "Hoja de derivación",
-    description: `Necesitamos que nos facilites una hoja de derivación para poder gestionar tu solicitud de registro. Los formatos aceptados son PDF, PNG o JPG.`,
+    description: `Necesitamos que nos facilites una hoja de derivación para poder gestionar tu solicitud de registro. Los formatos aceptados son PDF, PNG, JPG o JPEG.`,
     upload: "Subir imagen",
     camera: "Cámara",
     link: "No tengo hoja de derivación, pero tengo una cita con Servicios Sociales para su solicitud",
@@ -9,7 +9,7 @@ export const registerReferralEs = {
 
 export const registerReferralEn = {
     title: "Referral sheet",
-    description: `We need you to provide us with a referral sheet to be able to manage your registration request. The accepted formats are PDF, PNG or JPG.`,
+    description: `We need you to provide us with a referral sheet to be able to manage your registration request. The accepted formats are PDF, PNG, JPG or JPEG.`,
     upload: "Upload image",
     camera: "Camera",
     link: "I don't have a referral sheet, but I have an appointment with Social Services for its request",
