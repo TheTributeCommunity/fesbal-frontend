1787c3bfca8feecd6f7861f5aa6d040b5627853b
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-01-13 00:29:12 +0000

    Add cross(remove) icon and uploadImage icon

---
+++ src/components/icons/UploadImageIcon.tsx
@@ -0,0 +1,11 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowUpFromBracket} from "@fortawesome/free-solid-svg-icons";
+
+const UploadImageIcon = () => {
+
+    return (
+        <FontAwesomeIcon icon={faArrowUpFromBracket} size="2x" className="text-6xl text-quarter-color"/>
+    );
+}
+
+export default UploadImageIcon;
