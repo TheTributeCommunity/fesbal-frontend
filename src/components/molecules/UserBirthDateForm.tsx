4d2e9a4fdddc4b47d6a58b7a079c7f2a2b36e899
aufgfua <augustofalcaoflach@gmail.com>
2023-01-17 22:58:56 -0300

    feat(register): add primereact raw calendar

--- src/components/molecules/UserBirthDateForm.tsx
+++ src/components/molecules/UserBirthDateForm.tsx
@@ -2,12 +2,12 @@ import AppNextButton from "../atom/AppNextButton";
 import useUserBirthDateForm from "../../hooks/useUserBirthDateForm";
 import {useTranslation} from "react-i18next";
 import {namespaces} from "../../i18n/i18n.constants";
-//import {isLettersOnly} from "../../helpers/textUtils";
+import {Calendar} from "primereact/calendar";
 
 const UserBirthDateForm = () => {
     const {userBirthDate, onBirthDateChange, onSubmit, validateBirthDate} = useUserBirthDateForm();
     const {t: translation} = useTranslation(namespaces.pages.registrationBirthDate);
-
+     
     return (
         <form noValidate onSubmit={onSubmit}
               className="flex flex-col gap-4 h-full mt-6 self-center w-full md:w-1/2 lg:w-1/3 justify-between">
@@ -18,25 +18,16 @@ const UserBirthDateForm = () => {
                             className="text-secondary-color placeholder-primary-color rounded-md px-4 py-5 w-full"
                             value={userBirthDate} 
                             onChange={(e) => {
-                                // if(!isLettersOnly(e.target.value)) {
-                                //     return false;
-                                // }; 
                                 onBirthDateChange(e.target.value); 
                             }}/>
                 </div>
-                {/* <div className="flex flex-col gap-1.5 text-sm text-primary-color w-full">
-                    <label htmlFor="identityNumber" className={userSurname ? "opacity-100" : "opacity-0"}>{translation("surname")}</label>
-                    <input type="text" placeholder={translation("surname") as string}
-                            className="text-secondary-color placeholder-primary-color rounded-md px-4 py-5 w-full"
-                            value={userSurname}
-                            onChange={(e) => {
-                                if(!isLettersOnly(e.target.value)) {
-                                    return false;
-                                }; 
-                                onSurnameChange(e.target.value); 
-                            }}/>
-                </div> */}
             </div>
+            <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-1.5 text-sm text-primary-color w-full">
+                    <Calendar inline value={userBirthDate} onChange={(e) => onBirthDateChange(e.value?.toString() || "" )}></Calendar>
+                </div>
+            </div>
+ 
             <AppNextButton disabled={!validateBirthDate()} title={translation("next")}/>
         </form>
     );
