b9bc7f881a3179e0c8f6e40c839db4283194aed2
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-01-20 02:18:14 +0000

    Commit: Refactor and update components
    Removed unnecessary components: MenuReferralUploadForm and RegisterReferralForm, as we now have a common component: AppReferralForm
    Renamed all 't' variables to 'translate' for better readability
    Updated all components with new custom classes app-page and app-page__container
    Updated new forms to use new component AppFormInput
    Updated components to have correct links following Figma wireframes
    Fixed color issue with cancel button on AppPopupAlert

--- src/components/molecules/MenuReferralUploadForm.tsx
+++
@@ -1,9 +0,0 @@
-import AppReferralForm from "./AppReferralForm";
-
-const MenuReferralUploadForm = () => {
-    return (
-        <AppReferralForm link="/referral"/>
-    )
-}
-
-export default MenuReferralUploadForm;
--- src/components/molecules/RegisterReferralForm.tsx
+++
@@ -1,9 +0,0 @@
-import AppReferralForm from "./AppReferralForm";
-
-const RegisterReferralForm = () => {
-    return (
-        <AppReferralForm link="/register/request-sent" showSublink/>
-    )
-}
-
-export default RegisterReferralForm;
