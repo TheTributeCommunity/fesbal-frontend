198758a6d50898bcf735fabe82aec5ad70262814
melinahernandeztheam <melinahernandez@theagilemonkeys.com>
2023-01-23 14:12:01 +0100

    feat(uploadReferralSheet): connect component to RecipientUserService

--- src/mocks/users.mock.ts
+++ src/mocks/users.mock.ts
@@ -2,6 +2,7 @@ import UserProps from "../types/UserProps";
 
 const users: UserProps[] = [
     {
+        recipientUserId: "680381d0-43d7-46c3-966a-eec00593f0e7",
         id: "12345678A",
         password: "password1",
         email: "test1@gmail.com",
@@ -23,6 +24,7 @@ const users: UserProps[] = [
 
     },
     {
+        recipientUserId: "954f0389-2699-4963-b973-01fd11f16377",
         id: "12345678B",
         password: "password2",
         email: "test2@gmail.com",
@@ -41,6 +43,7 @@ const users: UserProps[] = [
         ]
     },
     {
+        recipientUserId: "87a4a581-8fea-457c-ad98-6b8aae9a0d91",
         id: "X2345678X",
         password: "password3",
         email: "test3@gmail.com",
@@ -59,6 +62,7 @@ const users: UserProps[] = [
         ]
     },
     {
+        recipientUserId: "bbcd1a9e-da49-4bd6-8320-f7e2787f8438",
         id: "Z2345678Z",
         password: "password4",
         email: "test4@gmail.com",
@@ -73,6 +77,7 @@ const users: UserProps[] = [
         ]
     },
     {
+        recipientUserId: "5ac1a155-0809-4904-9255-b5660da83771",
         id: "12345678L",
         password: "password5",
         email: "test5@gmail.com",
