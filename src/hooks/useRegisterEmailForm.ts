208fe9151c2cdb7409b9a4115d41ed85278288ee
Augusto Falcão Flach <augustofalcaoflach@gmail.com>
2023-01-25 10:48:34 -0300

    Update src/hooks/useRegisterEmailForm.ts
    Co-authored-by: Tai Nguyen Bui <tnb1990@gmail.com>

--- src/hooks/useRegisterEmailForm.ts
+++ src/hooks/useRegisterEmailForm.ts
@@ -24,7 +24,7 @@ const useRegisterEmailForm = () => {
             return false;
         }
 
-        if(validationInput === null){
+        if (validationInput === null) {
             validationInput = createValidationInput();
         }
 
