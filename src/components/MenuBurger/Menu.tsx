fae04ef0a07a03b6df3a0f4de5432e10ff036198
Alejandro Garcia <alegarp14@gmail.com>
2023-01-25 10:04:25 +0000

    Added Menu closing animation.

--- src/components/MenuBurger/Menu.tsx
+++ src/components/MenuBurger/Menu.tsx
@@ -1,19 +1,30 @@
-import MenuLink from './MenuLink';
-import Profile from './SVG_images/Profile';
-import Notification from './SVG_images/Notification';
-import Clock from './SVG_images/Clock';
-import VectorImg from './SVG_images/Vector';
-import Document from './SVG_images/Paper';
-import Help from './SVG_images/Help';
-import DeleteAccount from './SVG_images/DeleteAccount';
-import TurnOff from './SVG_images/TurnOff';
-import Cross from './SVG_images/Cross';
+import MenuLink from "./MenuLink";
+import Profile from "./SVG_images/Profile";
+import Notification from "./SVG_images/Notification";
+import Clock from "./SVG_images/Clock";
+import VectorImg from "./SVG_images/Vector";
+import Document from "./SVG_images/Paper";
+import Help from "./SVG_images/Help";
+import DeleteAccount from "./SVG_images/DeleteAccount";
+import TurnOff from "./SVG_images/TurnOff";
+import Cross from "./SVG_images/Cross";
 
-import '../../styles/components/Menu.scss';
+import "../../styles/MenuBurger/Menu.scss";
 
-export type Props =   {
-  callback : Function;
-}
+export type Props = {
+  callback: Function;
+};
+
+const defaultProps: Props = {
+  callback: () => {
+    let menu = document.getElementsByClassName("menu");
+    if (menu[0] != null) {
+      menu[0].classList.remove("close");
+      menu[0].classList.remove("open");
+      menu[0].classList.add("close");
+    }
+  },
+};
 
 /**
  * Creates a menu with a given function that is used on the
@@ -21,27 +32,37 @@ export type Props =   {
  * @param {Props} param0 callback function of the button
  * @returns container with the menu
  */
-const Menu = ({callback} : Props) => {
+const Menu = ({ callback }: Props) => {
   return (
-    <div className='menu'>
-      <div className='title'>
-        FESBAL
+    <div className="menu">
+      <div className="title">FESBAL</div>
+      <button className="x-button" onClick={() => callback()}>
+        {Cross()}
+      </button>
+      <div className="main-menu">
+        <MenuLink svgFile={Profile()} link="#" linkName="Perfil" />
+        <MenuLink svgFile={Notification()} link="#" linkName="Notificaciones" />
+        <MenuLink
+          svgFile={Clock()}
+          link="#"
+          linkName="Histórico de recogidas"
+        />
+        <MenuLink svgFile={VectorImg()} link="#" linkName="Punto de recogida" />
+        <MenuLink svgFile={Document()} link="#" linkName="Hoja de derivación" />
+        <MenuLink svgFile={Help()} link="#" linkName="Ayuda" />
+        <MenuLink
+          svgFile={DeleteAccount()}
+          link="#"
+          linkName="Eliminar cuenta"
+        />
       </div>
-      <button className='x-button' onClick={() => callback()}>{Cross()}</button>
-      <div className='main-menu'>
-        <MenuLink svgFile={Profile()} link="#" linkName="Perfil"/>
-        <MenuLink svgFile={Notification()} link="#" linkName="Notificaciones"/>
-        <MenuLink svgFile={Clock()} link="#" linkName="Histórico de recogidas"/>
-        <MenuLink svgFile={VectorImg()} link="#" linkName="Punto de recogida"/>
-        <MenuLink svgFile={Document()} link="#" linkName="Hoja de derivación"/>
-        <MenuLink svgFile={Help()} link="#" linkName="Ayuda"/>
-        <MenuLink svgFile={DeleteAccount()} link="#" linkName="Eliminar cuenta"/>
-      </div>
-      <div className='log-out'>
-        <MenuLink svgFile={TurnOff()} link="#" linkName="Log out"/>
+      <div className="log-out">
+        <MenuLink svgFile={TurnOff()} link="#" linkName="Log out" />
       </div>
     </div>
-  )
-}
+  );
+};
+
+Menu.defaultProps = defaultProps;
 
 export default Menu;
--- src/components/MenuBurger/MenuLink.tsx
+++ src/components/MenuBurger/MenuLink.tsx
@@ -1,23 +1,25 @@
-import '../../styles/components/MenuLink.scss';
+import "../../styles/MenuBurger/MenuLink.scss";
 
-export type Props =   {
-  svgFile : JSX.Element;
+export type Props = {
+  svgFile: JSX.Element;
   link: string;
-  linkName: string
-}
+  linkName: string;
+};
 
 /**
  * Creates a container with an image and a linked text
- * @param {Props} param0 {Image to show, Link to follow, Text to show} 
+ * @param {Props} param0 {Image to show, Link to follow, Text to show}
  * @returns HTML container
  */
-const MenuLink = ({svgFile, link, linkName} : Props) => {
-    return (
-      <div className='link-container'>
-        {svgFile}&emsp;
-        <a className='link-text' href={link}>{linkName}</a>
-      </div>
-    )
-}
+const MenuLink = ({ svgFile, link, linkName }: Props) => {
+  return (
+    <div className="link-container">
+      {svgFile}&emsp;
+      <a className="link-text" href={link}>
+        {linkName}
+      </a>
+    </div>
+  );
+};
 
-export default MenuLink
+export default MenuLink;
