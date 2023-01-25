898afae408bc06376a1df8b4ab400a751c904fa0
Juan Salvador <jsalvmaga@gmail.com>
2023-01-25 00:31:46 +0000

    Fixed merge issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,52 @@
-import { useState } from 'react';
-import './App.scss';
-import reactLogo from './assets/react.svg';
-
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import LoginPasswordRecovery from "./pages/LoginPasswordRecovery";
+import LoginScreen from "./pages/LoginScreen";
+import MenuReferral from "./pages/MenuReferral";
+import MenuReferralUpload from "./pages/MenuReferralUpload";
+import NotificationDetails from "./pages/NotificationDetails";
+import NotificationsScreen from "./pages/NotificationsScreen";
+import PickupPoint from "./pages/PickupPoint";
+import ProfileEditEmail from "./pages/ProfileEditEmail";
+import ProfileEditNewPassword from "./pages/ProfileEditNewPassword";
+import ProfileEditPrevPassword from "./pages/ProfileEditPrevPassword";
+import ProfileScreen from "./pages/ProfileScreen";
+import RegisterFamilyMembers from "./pages/RegisterFamilyMembers";
+import RegisterID from "./pages/RegisterID";
+import RegisterLegal from "./pages/RegisterLegal";
+import RegisterReferral from "./pages/RegisterReferral";
+import RegisterRequestSent from "./pages/RegisterRequestSent";
+import RegisterName from "./pages/RegisterName";
+import WelcomeScreen from "./pages/WelcomeScreen";
+import PickupHistoryPage from './pages/PickupHistoryPage';
 import NewFamilyMember from "./pages/NewFamilyMember";
 
 function App() {
-  const [count, setCount] = useState(0);
-  
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="*" element={<WelcomeScreen/>}/>
+                <Route path="/login" element={<LoginScreen/>}/>
+                <Route path="/login/password-recovery" element={<LoginPasswordRecovery/>}/>
+                <Route path="/notifications" element={<NotificationsScreen/>}/>
+                <Route path="/notifications/:id" element={<NotificationDetails/>}/>
+                <Route path="/pickup-point" element={<PickupPoint/>}/>
+                <Route path="/profile/*" element={<ProfileScreen/>}/>
+                <Route path="/profile/edit-email" element={<ProfileEditEmail/>}/>
+                <Route path="/profile/edit-new-password" element={<ProfileEditNewPassword/>}/>
+                <Route path="/profile/edit-prev-password" element={<ProfileEditPrevPassword/>}/>
+                <Route path="/referral" element={<MenuReferral/>}/>
+                <Route path="/referral/upload" element={<MenuReferralUpload/>}/>
+                <Route path="/register" element={<RegisterLegal/>}/>
+                <Route path="/register/family-members" element={<RegisterFamilyMembers/>}/>
+                <Route path="/register/id" element={<RegisterID/>}/>
+                <Route path="/register/name" element={<RegisterName/>}/>
+                <Route path="/register/referral" element={<RegisterReferral/>}/>
+                <Route path="/register/request-sent" element={<RegisterRequestSent/>}/>
+                <Route path="/welcome" element={<WelcomeScreen/>}/>
+                <Route path="/pickup-historic" element={<PickupHistoryPage/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import NewFamilyMember from './pages/NewFamilyMember';
-import './index.scss';
+import './styles/index.scss';
+import "./i18n/i18n";
+import { BoosterClient } from './services/booster-service';
+import { ApolloProvider } from '@apollo/client';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <NewFamilyMember />
+    <ApolloProvider client={BoosterClient}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
