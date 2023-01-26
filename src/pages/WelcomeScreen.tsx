e23fe3bb8386f634efeff2823a03dc2bb6bb422c
melinahernandeztheam <melinahernandez@theagilemonkeys.com>
2023-01-26 18:57:29 +0100

    feat(splash): Fix layout to match design

--- src/pages/WelcomeScreen.tsx
+++ src/pages/WelcomeScreen.tsx
@@ -1,6 +1,5 @@
 import {useTranslation} from "react-i18next";
 import {namespaces} from "../i18n/i18n.constants";
-import AppWrapper from "../components/molecules/AppWrapper";
 import PageHeader from "../components/molecules/AppPageHeader";
 import AppLinkButton from "../components/atom/AppLinkButton";
 
@@ -8,25 +7,33 @@ const WelcomeScreen = () => {
     const {t: translate} = useTranslation(namespaces.pages.welcomeScreen);
 
     return (
-        <AppWrapper title={translate("title")}>
+        <div>
+            <div className="flex flex-row fixed w-full md:w-1/2 lg:w-1/3 px-8 pt-4 pb-4 top-0 left-0 right-0 mx-auto">
+                <span
+                    className="font-roboto-flex font-bold text-base leading-6 text-dark-blue">BALPA</span>
+            </div>
             <PageHeader/>
-            <div className="h-full flex flex-col justify-between h-[calc(100vh-30rem)]">
-                <div className="text-secondary-color">
-                    <header className="font-big-title">{translate("recipient.title")}</header>
-                    <div>{translate("recipient.description")}</div>
-                    <footer>
-                        <AppLinkButton title={translate("recipient.login")} link="/register/name"/>
-                    </footer>
+            <div className="h-full flex flex-col justify-between h-[calc(100vh)] pt-16">
+                <div className="text-secondary-color h-1/2">
+                    <div className="mx-auto w-full md:w-1/2 lg:w-1/3 px-8 pt-8">
+                        <header className="font-big-title mb-4">{translate("recipient.title")}</header>
+                        <div>{translate("recipient.description")}</div>
+                        <footer className="pt-8">
+                            <AppLinkButton title={translate("recipient.login")} link="/register/name"/>
+                        </footer>
+                    </div>
                 </div>
-                <div className="bg-[#0F95CE] text-white">
-                    <header className="font-big-title">{translate("entity.title")}</header>
-                    <div>{translate("entity.description")}</div>
-                    <footer>
-                        <AppLinkButton bgColor={'bg-secondary-color'} title={translate("entity.login")} link="/welcome"/>
-                    </footer>
+                <div className="bg-[#0F95CE] text-white h-1/2">
+                    <div className="mx-auto w-full md:w-1/2 lg:w-1/3 px-8 pt-16">
+                        <header className="font-big-title mb-4">{translate("entity.title")}</header>
+                        <div>{translate("entity.description")}</div>
+                        <footer className="pt-8">
+                            <AppLinkButton bgColor={'bg-secondary-color'} title={translate("entity.login")} link="/welcome"/>
+                        </footer>
+                    </div>
                 </div>
             </div>
-        </AppWrapper>
+        </div>
     );
 };
 
