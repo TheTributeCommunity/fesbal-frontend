f529634ed062333fbac28a06d59c457d6610ccdd
Melina Hernández <122277388+melinahernandeztheam@users.noreply.github.com>
2023-01-26 14:51:28 +0100

    fix(build): Fix User props (#89)

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -35,10 +35,7 @@ export class AuthService {
         AuthService.auth = getAuth();
     }
 
-    public static signIn(user: userProps) {
-        if(!user.email){
-            throw new Error("Please, provide a valid email")
-        }
-        return signInWithEmailAndPassword(AuthService.auth, user.email, user.password)
+    public static signIn(email: string , password: string) {
+        return signInWithEmailAndPassword(AuthService.auth, email, password)
     }
 }
