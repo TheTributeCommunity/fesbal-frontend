786e7be847029e8b4de00a3da6f0601e9972ed94
melinahernandeztheam <melinahernandez@theagilemonkeys.com>
2023-01-27 13:35:25 +0100

    feat(splash): Add figma prototype link to entities login button

--- src/components/atom/SplashCard.tsx
+++ src/components/atom/SplashCard.tsx
@@ -3,20 +3,28 @@ import SplashCardProps from "../../types/SplashCardProps";
 import {Link} from "react-router-dom";
 import {useTranslation} from "react-i18next";
 import {namespaces} from "../../i18n/i18n.constants";
+import AppLinkExternalButton from "./AppLinkExtenernalButton";
 
 const SplashCard = ({title, description, loginText, loginLink, backgroundColor, textColor, buttonBackGroundColor, hasRegisterLink = false}: SplashCardProps) => {
     const {t: translate} = useTranslation(namespaces.pages.welcomeScreen);
+    const isAExternalLink = (link: string) => {
+        return link.includes("http")
+    }
+
     return (
         <div className={`${backgroundColor} ${textColor} h-1/2`}>
             <div className="mx-auto w-full md:w-1/2 lg:w-1/3 px-8 pt-16">
                 <header className="font-big-title mb-4">{title}</header>
                 <div>{description}</div>
                 <footer className="pt-8">
-                    <AppLinkButton bgColor={buttonBackGroundColor} title={loginText} link={loginLink}/>
+                    { isAExternalLink(loginLink)
+                        ? <AppLinkExternalButton bgColor={buttonBackGroundColor} title={loginText} link={loginLink}/>
+                        : <AppLinkButton bgColor={buttonBackGroundColor} title={loginText} link={loginLink}/>
+                    }
                     {hasRegisterLink &&
-                        (<Link to="/register"
+                        <Link to="/register"
                                className="self-end underline font-small-link">{translate("registerLink")}
-                        </Link>)
+                        </Link>
                     }
                 </footer>
             </div>
--- src/components/molecules/Splash.tsx
+++ src/components/molecules/Splash.tsx
@@ -5,6 +5,7 @@ import SplashCard from "../atom/SplashCard";
 
 const Splash = () => {
     const {t: translate} = useTranslation(namespaces.pages.welcomeScreen);
+    const FIGMA_PROTOTYPE_ENTITIES_LOGIN_URL = "https://www.figma.com/proto/fieW8OoWlFLyL90UleIeQ1/Draft-0.6---FESBAL?page-id=1147%3A14538&node-id=1147%3A15887&viewport=4483%2C1878%2C0.53&scaling=min-zoom&starting-point-node-id=1147%3A15887"
 
     return (
         <div className="h-full flex flex-col justify-between h-[calc(100vh-2rem)] mt-8">
@@ -25,7 +26,7 @@ const Splash = () => {
                 title={translate("entity.title")}
                 description={translate("entity.description")}
                 loginText={translate("entity.login")}
-                loginLink={"/"}
+                loginLink={FIGMA_PROTOTYPE_ENTITIES_LOGIN_URL}
                 backgroundColor={"bg-[#0F95CE]"}
                 textColor={"text-white"}
                 buttonBackGroundColor={"bg-secondary-color"}
