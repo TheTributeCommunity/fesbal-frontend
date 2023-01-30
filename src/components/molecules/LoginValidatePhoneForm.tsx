60545a5f64f473db2ef8d7afaed04041cdd4bc03
aufgfua <augustofalcaoflach@gmail.com>
2023-01-30 14:42:21 -0300

    feat(login validation): create login phone validation form and hooks

---
+++ src/components/molecules/LoginValidatePhoneForm.tsx
@@ -0,0 +1,40 @@
+import AppNextButton from "../atom/AppNextButton";
+import useLoginPhoneValidationForm from "../../hooks/useLoginValidatePhoneForm";
+import {useTranslation} from "react-i18next";
+import {namespaces} from "../../i18n/i18n.constants";
+import { AppDigitsInput } from "../atom/AppDigitsInput";
+import { FormEvent } from "react";
+
+interface LoginValidatePhoneProps {
+    onSubmit: (success: boolean) => void;
+}
+
+const LoginValidatePhoneForm = ({onSubmit: parentOnSubmit}: LoginValidatePhoneProps) => {
+    const {validationCode, CODE_LENGTH, checkValidationCodeLength, onValidationCodeChange, onSubmit} = useLoginPhoneValidationForm();
+    const {t: translate} = useTranslation(namespaces.pages.validatePhone);
+
+    const handleResendCode = () => {
+        // TODO implement resend code
+    };
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        // TODO implement submission and pass it to parent element
+        onSubmit(e);
+
+        parentOnSubmit(validationCode==="123456");
+    };
+
+    return (
+        <form noValidate onSubmit={handleSubmit} className="mt-4 flex w-full flex-col justify-between gap-4 self-center">
+            <div className="flex flex-col gap-4">
+                <AppDigitsInput label={translate("Código de validación")} 
+                                    digitsCount={CODE_LENGTH} onChange={onValidationCodeChange} 
+                                    value={validationCode} />
+            </div>
+            <p className="mt-4 mb-2 text-right text-secondary-color text-sm underline cursor-pointer font-semibold" onClick={handleResendCode}>{translate('resendCode')}</p>
+            <AppNextButton disabled={!checkValidationCodeLength()} title={translate("next")}/>
+        </form>
+    );
+}
+
+export default LoginValidatePhoneForm;
---
+++ src/hooks/useLoginValidatePhoneForm.ts
@@ -0,0 +1,41 @@
+import {FormEvent, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {AuthService} from "../services/auth-service";
+import {RecipientUserService} from "../services/recipient-user-service";
+import {UserGuestService} from "../services/user-guest-service";
+import {AppRoute} from "../enums/app-route";
+
+const useLoginValidatePhoneForm = () => {
+    const [validationCode, setValidationCode] = useState<string>('');
+    const CODE_LENGTH = 6;
+
+    const navigate = useNavigate();
+    const checkValidationCodeLength = (): boolean => {
+        return validationCode.length === CODE_LENGTH;
+    }
+
+    const onValidationCodeChange = (code: string) => {
+        setValidationCode(code);
+    }
+
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (checkValidationCodeLength()) {
+            /* Phone validation from registration step
+            AuthService.confirmPhoneCode(validationCode)
+                .then(() => RecipientUserService.create(UserGuestService.get()))
+                .then(() => navigate(AppRoute.REGISTER_FAMILY_MEMBERS))
+                */
+        }
+    }
+
+    return {
+        validationCode,
+        CODE_LENGTH,
+        checkValidationCodeLength,
+        onValidationCodeChange,
+        onSubmit
+    }
+}
+
+export default useLoginValidatePhoneForm;
