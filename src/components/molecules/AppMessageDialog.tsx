f3efbe31c5a27be7a404b446e033c7c2e742c013
Samuel Dominguez <samueldominguez@protonmail.com>
2023-01-30 14:23:32 +0000

    dialog width now responsive

--- src/components/molecules/AppMessageDialog.tsx
+++ src/components/molecules/AppMessageDialog.tsx
@@ -24,22 +24,22 @@ const AppMessageDialog: FC<AppMessageDialogProps> = ({
 }) => {
     return (
         <div className="absolute top-0 left-0 bg-secondary-color bg-opacity-40 z-50 w-full h-full">
-
-			<div className="absolute left-4 right-4 bottom-6 p-6 pt-10 text-center text-secondary-color font-roboto-flex bg-white rounded-2xl">
-				<div className="flex flex-col">
-                    <div className="mb-6 mx-auto">
-					    {icon}
+            <div className="absolute bottom-6 left-0 right-0 px-4 ">
+                <div className="w-full md:w-1/2 lg:w-1/3 p-6 pt-10 text-center text-secondary-color font-roboto-flex bg-white rounded-2xl mx-auto">
+                    <div className="flex flex-col">
+                        <div className="mb-6 mx-auto">
+                            {icon}
+                        </div>
+                        <h2 className={titleClassname + " mb-8 font-big-title"}>{title}</h2>
+                    
+                        <p className={descriptionClassname + " font-light mb-12 text-base"}>{description}</p>
+                    
                     </div>
-                    <h2 className={titleClassname + " mb-8 font-big-title"}>{title}</h2>
-                
-                    <p className={descriptionClassname + " font-light mb-12 text-base"}>{description}</p>
-                
-				</div>
-                <div>
-                    <AppNextButton title={buttonText} onClick={buttonOnClick} bgColor={buttonBgColor}/>
+                    <div>
+                        <AppNextButton title={buttonText} onClick={buttonOnClick} bgColor={buttonBgColor}/>
+                    </div>
                 </div>
-			</div>
-
+            </div>
 		</div>
     );
 };
