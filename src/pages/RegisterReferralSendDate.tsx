7aee88bfa25ff95852293ee8b2475a23733c45e5
aufgfua <augustofalcaoflach@gmail.com>
2023-02-01 11:30:55 -0300

    fix(referral send date): add referral page

---
+++ src/pages/RegisterReferralSendDate.tsx
@@ -0,0 +1,50 @@
+import {useTranslation} from "react-i18next";
+import {useState} from "react";
+import {namespaces} from "../i18n/i18n.constants";
+import AppReferralSendDateForm from "../components/molecules/AppReferralSendDateForm";
+import AppPageHeader from "../components/molecules/AppPageHeader";
+import AppWrapper from "../components/molecules/AppWrapper";
+import {AppRoute} from "../enums/app-route";
+import AppMessageDialog from "../components/molecules/AppMessageDialog";
+import AlertIcon from "../components/icons/AlertIcon";
+
+const RegisterReferralSendDate = () => {
+    const {t: translate} = useTranslation(namespaces.pages.registerReferralSendDate);
+    const [showConfirmDialog, setshowConfirmDialog] = useState(false);
+
+    const onSubmit = () => {
+        setshowConfirmDialog(true);
+    };
+
+    const selectSubmit = () => {
+        // TODO: submit form
+        setshowConfirmDialog(false);
+    };
+
+    const selectCancel = () => {
+        setshowConfirmDialog(false);
+    };
+
+
+    return (
+        <AppWrapper title={translate("headerTitle")} link={AppRoute.REGISTER_REFERRAL_SHEET}>
+            <div className="flex h-full w-full flex-col self-center text-secondary-color">
+                <AppPageHeader title={translate("title")} description={translate("description") as string}/>
+                <AppReferralSendDateForm onSubmit={onSubmit}/>
+            </div>
+            <AppMessageDialog
+                visible={showConfirmDialog}
+                icon={<AlertIcon />}
+                description={translate("adviceDescription")}
+                title={translate("adviceTitle")}
+                buttonText={translate("adviceConfirm")}
+                buttonOnClick={selectSubmit}
+                secondaryButtonText={translate("adviceCancel")}
+                secondaryButtonOnClick={selectCancel}
+                secondaryButtonBgColor="bg-warning-color"
+            />
+        </AppWrapper>
+    );
+}
+
+export default RegisterReferralSendDate;
