a4bffa413c46f92defb39d7f0b5474dd39be1d15
aufgfua <augustofalcaoflach@gmail.com>
2023-02-01 11:05:22 -0300

    refact(dialog): create visible property

--- src/components/molecules/AppMessageDialog.tsx
+++ src/components/molecules/AppMessageDialog.tsx
@@ -3,6 +3,7 @@ import AppNextButton from "../atom/AppNextButton";
 
 interface AppMessageDialogProps {
     icon?: React.ReactNode;
+    visible?: boolean;
     title: string;
     titleClassname?: string;
     description: string;
@@ -14,6 +15,7 @@ interface AppMessageDialogProps {
 
 const AppMessageDialog: FC<AppMessageDialogProps> = ({
     icon,
+    visible = true,
     title,
     titleClassname,
     description,
@@ -23,24 +25,28 @@ const AppMessageDialog: FC<AppMessageDialogProps> = ({
     buttonOnClick,
 }) => {
     return (
-        <div className="absolute top-0 left-0 bg-secondary-color bg-opacity-40 z-50 w-full h-full">
-            <div className="absolute bottom-6 left-0 right-0 px-4 ">
-                <div className="w-full md:w-1/2 lg:w-1/3 p-6 pt-10 text-center text-secondary-color font-roboto-flex bg-white rounded-2xl mx-auto">
-                    <div className="flex flex-col">
-                        <div className="mb-6 mx-auto">
-                            {icon}
+        <>
+            { visible &&
+                <div className="absolute top-0 left-0 bg-secondary-color bg-opacity-40 z-50 w-full h-full">
+                    <div className="absolute bottom-6 left-0 right-0 px-4 ">
+                        <div className="w-full md:w-1/2 lg:w-1/3 p-6 pt-10 text-center text-secondary-color font-roboto-flex bg-white rounded-2xl mx-auto">
+                            <div className="flex flex-col">
+                                <div className="mb-6 mx-auto">
+                                    {icon}
+                                </div>
+                                <h2 className={titleClassname + " mb-8 font-big-title"}>{title}</h2>
+                            
+                                <p className={descriptionClassname + " font-light mb-12 text-base"}>{description}</p>
+                            
+                            </div>
+                            <div>
+                                <AppNextButton title={buttonText} onClick={buttonOnClick} bgColor={buttonBgColor}/>
+                            </div>
                         </div>
-                        <h2 className={titleClassname + " mb-8 font-big-title"}>{title}</h2>
-                    
-                        <p className={descriptionClassname + " font-light mb-12 text-base"}>{description}</p>
-                    
-                    </div>
-                    <div>
-                        <AppNextButton title={buttonText} onClick={buttonOnClick} bgColor={buttonBgColor}/>
                     </div>
                 </div>
-            </div>
-		</div>
+            }
+        </>
     );
 };
 
