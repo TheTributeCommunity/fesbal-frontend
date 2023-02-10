dd7f2fc34f9cc4eca9dda92894a25aa3da8e2345
aufgfua <augustofalcaoflach@gmail.com>
2023-02-10 12:51:47 -0300

    refact(pickup): change mock and types structure

--- src/types/Pickup.ts
+++ src/types/Pickup.ts
@@ -2,13 +2,15 @@ import { PickupItem } from "./PickupItem";
 
 export interface Pickup {
   id: number;
+  entityName: string;
   date: string;
   title: string;
   pickupItems: PickupItem[];
 }
 
 export const getPickupDescription = (pickup: Pickup) => { 
-  const itemsStr = pickup.pickupItems.map(item => `${item.quantity} ${item.unity.unity.toLowerCase()} de ${item.food.name}`).join(', ');
-  
-  return `${itemsStr}`;
+  const itemsStr = pickup.pickupItems.map(
+    item => `${item.quantity} ${item.unity.unity.toLowerCase()} de ${item.food.name}`)
+    .join(', ');
+  return itemsStr;
 }
