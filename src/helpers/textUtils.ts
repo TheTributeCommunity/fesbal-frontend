c5160a7aa9033ee8583fd3a5e0d53f5eadefa4c3
aufgfua <augustofalcaoflach@gmail.com>
2023-02-10 12:52:32 -0300

    feat(utils): create simple formatting date function

--- src/helpers/textUtils.ts
+++ src/helpers/textUtils.ts
@@ -2,3 +2,20 @@ export const isLettersOnly = (text: string) => {
     const onlyLettersText = text.match(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ~^´` ]*$/ig) || [];
     return onlyLettersText.length == 1;
 }
+
+export const dateToDdMmYyyy = (date: Date) => {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+}
+
+export const dateToHoursMinutes = (date: Date) => {
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
+    const ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    
+    return `${hours}:${minutes} ${ampm}`;
+}
--- src/mocks/history.mock.ts
+++ src/mocks/history.mock.ts
@@ -5,29 +5,29 @@ export const HistoryMock: Pickup[] = [
   {
     id: 1,
     date: '2022-12-23T10:30:00.000Z',
+    entityName: 'Entity 1',
     title: 'History 1',
-    description: 'History 1 description',
     pickupItems: PickupItemMock,
   },
   {
     id: 2,
     date: '2022-12-24T10:30:00.000Z',
+    entityName: 'Entity 2',
     title: 'History 2',
-    description: 'History 2 description',
     pickupItems: PickupItemMock,
   },
   {
     id: 3,
     date: '2022-12-25T10:30:00.000Z',
+    entityName: 'Entity 3',
     title: 'History 3',
-    description: 'History 3 description',
     pickupItems: PickupItemMock,
   },
   {
     id: 4,
     date: '2022-12-26T10:30:00.000Z',
+    entityName: 'Entity 4',
     title: 'History 4',
-    description: 'History 4 description',
     pickupItems: PickupItemMock,
   },
 ];
