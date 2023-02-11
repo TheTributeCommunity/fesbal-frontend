4ca3208e31a3046e1cf0ff8c2082182894a76e4b
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-02-11 18:02:01 +0000

    Add i18n text for EntityHome

--- src/i18n/i18n.constants.ts
+++ src/i18n/i18n.constants.ts
@@ -1,5 +1,6 @@
 export const namespaces = {
     pages: {
+        entityHome: "pages.entityHome",
         entityLogin: "pages.entityLogin",
         entityLoginPasswordRecovery: "pages.entityLoginPasswordRecovery",
         loginScreen: "pages.loginScreen",
