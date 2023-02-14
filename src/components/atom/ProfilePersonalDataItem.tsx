bd20dd523e85ebb29934e9cf2412ac455c914d1c
aufgfua <augustofalcaoflach@gmail.com>
2023-02-14 02:59:53 -0300

    refact(layout): fix profile screen layout

--- src/components/atom/ProfilePersonalDataItem.tsx
+++ src/components/atom/ProfilePersonalDataItem.tsx
@@ -1,27 +1,33 @@
-import AppEditProfileButton from "./AppEditProfileButton";
-import PersonalDataItemProps from "../../types/PersonalDataItemProps";
+import PersonalDataItemProps from '../../types/PersonalDataItemProps'
+import AppEditProfileButton from './AppEditProfileButton'
 
-const ProfilePersonalDataItem = ({personalData, index}: { personalData: PersonalDataItemProps, index: number }) => {
-    const {title, value, hasEditButton, goTo} = personalData;
-    const bgColor = index % 2 === 0 ? "" : "bg-white";
+interface ProfilePersonalDataItemProps {
+    personalData: PersonalDataItemProps
+    index: number
+    itemClassName?: string
+}
+
+const ProfilePersonalDataItem = ({personalData, index, itemClassName}: ProfilePersonalDataItemProps) => {
+    const {title, value, hasEditButton, goTo} = personalData
+    const bgColor = index % 2 === 0 ? '' : 'bg-white'
     return (
         <>
             {hasEditButton ?
-                <li className={`flex flex-row justify-between items-center ${bgColor} rounded-md p-4`}>
+                <li className={`flex flex-row justify-between items-center ${bgColor} lg:rounded-md p-4  min-h-[4.5rem] ${itemClassName}`}>
                     <div className="flex flex-col gap-1">
                         <p className="text-primary-color font-label">{title}</p>
-                        <p className="text-secondary-color font-input">{value}</p>
+                        <p className="text-secondary-color font-input mt-1">{value}</p>
                     </div>
                     {goTo && <AppEditProfileButton goTo={goTo}/>}
                 </li>
                 :
-                <li className={`${bgColor} rounded-md p-4`}>
+                <li className={`${bgColor} lg:rounded-md p-4 min-h-[4.5rem] ${itemClassName}`}>
                     <p className="text-primary-color font-label">{title}</p>
-                    <p className="text-secondary-color font-input">{value}</p>
+                    <p className="text-secondary-color font-input mt-1">{value}</p>
                 </li>
             }
         </>
-    );
-};
+    )
+}
 
-export default ProfilePersonalDataItem;
+export default ProfilePersonalDataItem
