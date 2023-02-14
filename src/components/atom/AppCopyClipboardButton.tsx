5d7607f2b5ed1bf5f626cbb5152fd9bf618d5a76
aufgfua <augustofalcaoflach@gmail.com>
2023-02-14 03:43:02 -0300

    refact(design): change colors to match pickup point design

--- src/components/atom/AppCopyClipboardButton.tsx
+++ src/components/atom/AppCopyClipboardButton.tsx
@@ -1,14 +1,14 @@
-import CopyClipboardIcon from "../icons/CopyClipboardIcon";
+import CopyClipboardIcon from '../icons/CopyClipboardIcon'
 
 const AppCopyClipboardButton = ({text}: { text: string }) => {
     return (
         <div className="flex flex-row justify-between">
-            <p className="font-input">{text}</p>
+            <p className="font-input text-secondary-color pr-1">{text}</p>
             <button onClick={() => navigator.clipboard.writeText(text)}>
                 <CopyClipboardIcon/>
             </button>
         </div>
-    );
+    )
 }
 
-export default AppCopyClipboardButton;
+export default AppCopyClipboardButton
