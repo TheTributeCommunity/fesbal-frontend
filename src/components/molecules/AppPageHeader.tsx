e0f31880fb89be5c384ccf3e73a06ac3c6d89793
aufgfua <augustofalcaoflach@gmail.com>
2023-02-15 12:35:05 -0300

    feat(i18n): update texts to match v.6 design

--- src/components/molecules/AppPageHeader.tsx
+++ src/components/molecules/AppPageHeader.tsx
@@ -3,12 +3,14 @@ interface PageHeaderProps {
     description?: string | null;
 }
 
-export default ({title, description}: PageHeaderProps) => {
+const PageHeaderProps = ({title, description}: PageHeaderProps) => {
     return (
         <div>
             {title && <h1 className="mb-4 font-big-title text-secondary-color">{title}</h1>}
             <p className="font-text text-secondary-color">{description}</p>
         </div>
-    );
-};
+    )
+}
+
+export default PageHeaderProps
 
