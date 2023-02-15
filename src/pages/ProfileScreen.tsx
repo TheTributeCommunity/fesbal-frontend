278b66d263dff85567ef4bdb579e7d57998c4ae3
Augusto Falcão Flach <augustofalcaoflach@gmail.com>
2023-02-15 13:05:18 -0300

    [screen-10-18-28-31-33-35 v.6, issue 135] Fix small design differences and colors (#136)
    * refact(layout): fix profile screen layout
    
    * refact(code structure): remove !important from style
    
    * fix(style): fix profile text color
    
    * refact(layout): change pickup history to match design
    
    * refact(layout): change layout to match request-sent design
    
    * refact(design): change home text colors
    
    * refact(design): change colors to match pickup point design
    
    * fix(spacing): change pickup point spacing to match design
    
    ---------
    
    Co-authored-by: Samuel Domínguez Lorenzo <samueldominguez@protonmail.com>

--- src/pages/ProfileScreen.tsx
+++ src/pages/ProfileScreen.tsx
@@ -1,19 +1,16 @@
-import PersonalDataItemProps from "../types/PersonalDataItemProps";
-import ProfilePersonalDataItem from "../components/atom/ProfilePersonalDataItem";
-import {namespaces} from "../i18n/i18n.constants";
-import {useTranslation} from "react-i18next";
-import FamilyMembersIcon from "../components/icons/FamilyMembersIcon";
-import AppWrapper from "../components/molecules/AppWrapper";
-import FamilyMemberCard from "../components/atom/FamilyMemberCard";
-import useRegisterFamilyMembers from "../hooks/useRegisterFamilyMembers";
-import EditIcon from "../components/icons/EditIcon";
-import AppEditProfileButton from "../components/atom/AppEditProfileButton";
-import { AppRoute } from "../enums/app-route";
+import { useTranslation } from 'react-i18next'
+import FamilyMemberCard from '../components/atom/FamilyMemberCard'
+import ProfilePersonalDataItem from '../components/atom/ProfilePersonalDataItem'
+import FamilyMembersIcon from '../components/icons/FamilyMembersIcon'
+import AppWrapper from '../components/molecules/AppWrapper'
+import useRegisterFamilyMembers from '../hooks/useRegisterFamilyMembers'
+import { namespaces } from '../i18n/i18n.constants'
+import PersonalDataItemProps from '../types/PersonalDataItemProps'
 
 
 const ProfileScreen = () => {
-    const {t: translate} = useTranslation(namespaces.pages.profileScreen);
-    const {user, familyMembers} = useRegisterFamilyMembers();
+    const {t: translate} = useTranslation(namespaces.pages.profileScreen)
+    const {user, familyMembers} = useRegisterFamilyMembers()
     const getPersonalData = (): PersonalDataItemProps[] => {
         return [
             {
@@ -31,8 +28,7 @@ const ProfileScreen = () => {
             {
                 title: translate('email'),
                 value: user?.email,
-                hasEditButton: true,
-                goTo: `/profile/edit-email`,
+                goTo: '/profile/edit-email',
             },
             {
                 title: translate('phone'),
@@ -40,34 +36,33 @@ const ProfileScreen = () => {
             },
             {
                 title: translate('password'),
-                value: "********",
-                hasEditButton: true,
-                goTo: `/profile/edit-prev-password`,
+                value: '********',
+                goTo: '/profile/edit-prev-password',
             }
-        ];
-    };
+        ]
+    }
 
     return (
-        <AppWrapper showBackButton title={translate('title')} showBurger>
+        <AppWrapper showBackButton title={translate('title')} containerClassName="px-0" showBurger>
             <div className="flex w-full flex-col gap-4 mb-2">
-                <ul>
+                <ul className='border-b-[1px] border-b-primary-color'>
                     {getPersonalData().map((personalData, index) => (
-                        <ProfilePersonalDataItem key={index} personalData={personalData} index={index}/>
+                        <ProfilePersonalDataItem key={index} personalData={personalData} index={index} itemClassName="px-8"/>
                     ))}
                 </ul>
-                <div className="flex flex-row items-center gap-2 pl-2 font-bold mt-8">
+                <div className="flex flex-row items-center gap-2 px-8 font-bold mt-3">
                     <FamilyMembersIcon/>
-                    <h2 className="font-mini-title">{translate('familyMembers')}</h2>
-                    <div className="ml-auto"><AppEditProfileButton goTo={AppRoute.REGISTER_FAMILY_MEMBERS} /></div>
+                    <h2 className="font-mini-title text-secondary-color">{translate('familyMembers')}</h2>
+                    <div className="ml-auto"></div>
                 </div>
                 {familyMembers.map((familyMember, index) => 
-                    <div className="flex flex-col gap-4 pb-2">
+                    <div className="flex flex-col gap-4 pb-2" key={index}>
                         <span className="text-primary-color font-roboto-flex font-bold text-base leading-5">{translate('familyMember', {ns: 'pages.registerFamilyMembers'})} {index+1}</span>
                         <FamilyMemberCard person={familyMember} />
                     </div>)}
             </div>
         </AppWrapper>
-    );
+    )
 }
 
-export default ProfileScreen;
+export default ProfileScreen
--- src/pages/RegisterRequestSent.tsx
+++ src/pages/RegisterRequestSent.tsx
@@ -1,27 +1,29 @@
-import AppLinkButton from "../components/atom/AppLinkButton";
-import {useTranslation} from "react-i18next";
-import {namespaces} from "../i18n/i18n.constants";
-import LogoFesbalWhiteIcon from "../components/icons/LogoFesbalWhiteIcon";
-import RequestSentIcon from "../components/icons/RequestSentIcon";
-import UsersMock from "../mocks/users.mock";
-import users from "../mocks/users.mock";
-import PersonalDataItemProps from "../types/PersonalDataItemProps";
-import RegisterPersonalDataItem from "../components/atom/RegisterPersonalDataItem";
-import {AppRoute} from "../enums/app-route";
+import { useTranslation } from 'react-i18next'
+import AppLinkButton from '../components/atom/AppLinkButton'
+import RegisterPersonalDataItem from '../components/atom/RegisterPersonalDataItem'
+import LogoFesbalWhiteIcon from '../components/icons/LogoFesbalWhiteIcon'
+import RequestSentIcon from '../components/icons/RequestSentIcon'
+import { AppRoute } from '../enums/app-route'
+import { namespaces } from '../i18n/i18n.constants'
+import { default as users, default as UsersMock } from '../mocks/users.mock'
+import PersonalDataItemProps from '../types/PersonalDataItemProps'
 
 const RegisterRequestSent = () => {
-    const {t: translate} = useTranslation(namespaces.pages.registerRequestSent);
-    const user = UsersMock[0];
+    const {t: translate} = useTranslation(namespaces.pages.registerRequestSent)
+    const user = UsersMock[0]
+
+
     const getFamilyMembers = (id: string) => {
-        const user = users.find(user => user.id === id);
-        return user?.familyMembers;
+        const user = users.find(user => user.id === id)
+        return user?.familyMembers
     }
     const getPersonalData = (): PersonalDataItemProps[] => {
         return [
             {
                 title: translate('fullName'),
                 value: user.fullName,
-                span: 2
+                span: 2,
+                className: 'font-bold'
             },
             {
                 title: translate('id'),
@@ -37,7 +39,7 @@ const RegisterRequestSent = () => {
                 title: translate('email'),
                 value: user.email,
                 hasEditButton: true,
-                goTo: `/profile/edit-email`,
+                goTo: '/profile/edit-email',
                 span: 1
             },
             {
@@ -50,28 +52,28 @@ const RegisterRequestSent = () => {
                 value: getFamilyMembers(user.id)?.length.toString() || '0',
                 span: 2
             }
-        ];
-    };
+        ]
+    }
 
     return (
-        <div className="h-screen flex flex-col justify-end bg-primary-color px-4 pb-4">
-            <div className="app-logo">
+        <div className="min-h-screen flex flex-col justify-between bg-primary-color px-4 pb-4">
+            <div className="pb-8 pt-14">
                 <LogoFesbalWhiteIcon/>
             </div>
-            <div className="mx-auto w-full md:w-1/2 lg:w-1/3 px-8 pb-4 text-center bg-white rounded-xl text-secondary-color z-10">
+            <div className="app-shadow mx-auto w-full md:w-1/2 lg:w-1/3 px-8 pt-6 pb-4 text-center bg-white rounded-xl text-secondary-color z-10">
                 <RequestSentIcon/>
-                <div className="flex flex-col">
-                    <h1 className="mb-4 font-big-title">{translate("title")}</h1>
-                    <ul className="grid grid-cols-2 gap-3 pb-2 text-left justify-between">
+                <div className="flex flex-col mt-4">
+                    <h1 className="mb-4 font-big-title font-roboto-flex">{translate('title')}</h1>
+                    <ul className="grid grid-cols-2 gap-x-3 gap-y-4 pb-2 text-left justify-between">
                         {getPersonalData().map((item, index) => (
-                            <RegisterPersonalDataItem title={item.title} value={item.value} span={item.span} key={index}/>
+                            <RegisterPersonalDataItem title={item.title} value={item.value} span={item.span} key={index} className={item.className}/>
                         ))}
                     </ul>
                 </div>
-                <AppLinkButton title={translate("next")} link={AppRoute.WELCOME}/>
+                <AppLinkButton title={translate('next')} link={AppRoute.WELCOME}/>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default RegisterRequestSent;
+export default RegisterRequestSent
