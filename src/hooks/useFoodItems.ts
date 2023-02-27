7141e9c4b58dfe4c11cad20494b320db3401249f
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-02-27 09:55:25 +0000

    Update useFoodItems hook

--- src/hooks/useFoodItems.ts
+++ src/hooks/useFoodItems.ts
@@ -1,27 +1,45 @@
-import FamilyUnitAgesProps from "../types/FamilyUnitAgesProps";
-import {useEffect, useState} from "react";
-import FoodItemProps from "../types/FoodItemProps";
-import getDefaultFoodItems from "../helpers/getDefaultFoodItems";
+import {useState} from 'react'
+import FoodItemProps from '../types/FoodItemProps'
+import FoodNames from '../enums/food-names'
+import {MeasurementUnit} from '../enums/measurement'
+import {useNavigate} from 'react-router'
+import {AppRoute} from '../enums/app-route'
 
-const useFoodItems = (familyAgeCount: FamilyUnitAgesProps, userFoodItems: FoodItemProps[] = []) => {
-    const [foodItems, setFoodItems] = useState<FoodItemProps[]>(userFoodItems);
-
-    useEffect(() => {
-        if (userFoodItems.length === 0) {
-            setFoodItems(getDefaultFoodItems(familyAgeCount));
-        }
-    }, []);
-
-    const addFoodItem = (foodItem: FoodItemProps) => {
-        setFoodItems((items) => [...items, foodItem]);
-    };
-
+const useFoodItems = (userFoodItems: FoodItemProps[] = []) => {
+    const [foodItems, setFoodItems] = useState<FoodItemProps[]>(userFoodItems)
+    const navigate = useNavigate()
     const removeFoodItem = (foodItem: FoodItemProps) => {
-        setFoodItems(foodItems.filter((item) => item !== foodItem));
-    };
+        setFoodItems(foodItems.filter(item => item.name !== foodItem.name))
+    }
+    const updateFoodItem = (foodName: FoodNames, oldFoodName: FoodNames) => {
+        const foodNameAlreadyExists = foodItems.some(item => item.name === foodName)
+        if (foodNameAlreadyExists) {
+            const removedFoodItems = foodItems.filter(item => item.name !== oldFoodName)
+            setFoodItems(removedFoodItems)
+            navigate(AppRoute.ENTITY_USER_SCANNED, {state: {foodItems: removedFoodItems}})
+            return
+        }
+        const updatedFoodItems = foodItems.map(item => {
+            if (item.name === oldFoodName) return {...item, name: foodName}
+            return item
+        })
+        setFoodItems(updatedFoodItems)
+        navigate(AppRoute.ENTITY_USER_SCANNED, {state: {foodItems: updatedFoodItems}})
+    }
 
-    const updateFoodItem = (foodItem: FoodItemProps) => {
-        setFoodItems(foodItems.map((item) => item === foodItem ? foodItem : item));
+    const addFoodItem = (foodName: FoodNames) => {
+        if (foodItems.some(item => item.name === foodName)) {
+            navigate(AppRoute.ENTITY_USER_SCANNED, {state: {foodItems: foodItems}})
+            return
+        }
+        const newFoodItem: FoodItemProps = {
+            name: foodName,
+            quantity: 1,
+            measurementUnit: MeasurementUnit.UNITS
+        }
+        const updatedFoodItems = [...foodItems, newFoodItem]
+        setFoodItems(updatedFoodItems)
+        navigate(AppRoute.ENTITY_USER_SCANNED, {state: {foodItems: updatedFoodItems}})
     }
 
     return {
@@ -29,7 +47,7 @@ const useFoodItems = (familyAgeCount: FamilyUnitAgesProps, userFoodItems: FoodIt
         addFoodItem,
         removeFoodItem,
         updateFoodItem,
-    };
-};
+    }
+}
 
-export default useFoodItems;
+export default useFoodItems
