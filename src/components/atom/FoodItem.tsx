96fe5f7b170319c6e9a97688a18015a3648f1ad0
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-02-27 09:54:16 +0000

    Update FoodItem component

--- src/components/atom/FoodItem.tsx
+++ src/components/atom/FoodItem.tsx
@@ -1,20 +1,22 @@
-import CrossIcon from "../icons/CrossIcon";
-import FoodItemProps from "../../types/FoodItemProps";
-import {Link} from "react-router-dom";
-import {AppRoute} from "../../enums/app-route";
+import CrossIcon from '../icons/CrossIcon'
+import FoodItemProps from '../../types/FoodItemProps'
+import {Link} from 'react-router-dom'
+import {AppRoute} from '../../enums/app-route'
 
 interface FoodItemsComponent extends FoodItemProps {
     onRemove: () => void;
+    onFoodClick: () => void;
 }
 
-const FoodItem = ({name, quantity, measurementUnit, onRemove}: FoodItemsComponent) => {
+const FoodItem = ({name, quantity, measurementUnit, onRemove, onFoodClick}: FoodItemsComponent) => {
     return (
         <li className="flex gap-4 items-center mb-2 justify-between">
-            <Link to={AppRoute.ENTITY_LOGIN} className="text-secondary-color font-input w-7/12 bg-white rounded-lg p-4">
+            <div onClick={onFoodClick}
+                className="text-secondary-color font-input w-7/12 bg-white rounded-lg p-4 cursor-pointer">
                 <p>{name}</p>
-            </Link>
-            <Link to={AppRoute.ENTITY_LOGIN}
-                  className="text-secondary-color font-input w-3/12 bg-white rounded-lg p-4">
+            </div>
+            <Link to={AppRoute.ENTITY_FOOD_SEARCH}
+                className="text-secondary-color font-input w-3/12 bg-white rounded-lg p-4">
                 <p className="flex justify-center items-baseline gap-1">
                     {quantity}
                     <span className="text-primary-color font-label">{measurementUnit}</span>
@@ -24,7 +26,7 @@ const FoodItem = ({name, quantity, measurementUnit, onRemove}: FoodItemsComponen
                 <CrossIcon/>
             </button>
         </li>
-    );
-};
+    )
+}
 
-export default FoodItem;
+export default FoodItem
