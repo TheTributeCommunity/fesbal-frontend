4cd8dd1747fcf80619bf99b8e7d59d7840c7e6d9
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-03-03 18:43:03 +0000

    Add topbar property bg

--- src/components/molecules/AppWrapper.tsx
+++ src/components/molecules/AppWrapper.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames'
-import { ReactNode } from 'react'
+import {ReactNode} from 'react'
 import AppBackButton from '../atom/AppBackButton'
 import AppBurgerMenuButton from '../atom/AppBurgerMenuButton'
 
@@ -11,19 +11,31 @@ interface AppWrapperProps {
     showBackButton?: boolean
     children?: ReactNode,
     bgColor?: string
+    topbarClassName?: string
 }
 
-const AppWrapper = ({title, titleClassName = '', showBurger = false, showBackButton = true, children, containerClassName = '', bgColor = 'bg-page'} : AppWrapperProps): JSX.Element => {
+const AppWrapper = ({
+    title,
+    titleClassName = '',
+    showBurger = false,
+    showBackButton = true,
+    children,
+    containerClassName = '',
+    bgColor = 'bg-page',
+    topbarClassName
+}: AppWrapperProps): JSX.Element => {
     return (
         // We set the height to the screen, and then set the
         // height to 100dvh - this is because some browsers
         // do not yet support dynamic viewport, so in the event
         // it doesn't support it, it stays as viewport height
         <div className={`${bgColor} h-screen h-[100dvh]`}>
-            <div className={`flex flex-col justify-center mx-auto w-full md:w-1/2 lg:w-1/3 p-8 h-full ${containerClassName}`}>
-                <div className="flex flex-row fixed w-full md:w-1/2 lg:w-1/3 px-8 py-4 top-0 left-0 right-0 mx-auto bg-white">
+            <div
+                className={`flex flex-col justify-center mx-auto w-full md:w-1/2 lg:w-1/3 p-8 h-full ${containerClassName}`}>
+                <div
+                    className={`flex flex-row fixed w-full md:w-1/2 lg:w-1/3 px-8 py-4 top-0 left-0 right-0 mx-auto ${topbarClassName}`}>
                     <div className="w-1/4 flex flex-row justify-start">
-                        {showBackButton && <AppBackButton />}
+                        {showBackButton && <AppBackButton/>}
                     </div>
                     <div className="w-2/3 text-center flex flex-row justify-center items-center">
                         <span className={classNames({
@@ -32,7 +44,7 @@ const AppWrapper = ({title, titleClassName = '', showBurger = false, showBackBut
                         })}>{title}</span>
                     </div>
                     <div className="w-1/4 flex flex-row justify-end">
-                        {showBurger && <AppBurgerMenuButton />}
+                        {showBurger && <AppBurgerMenuButton/>}
                     </div>
                 </div>
                 <div className="flex flex-col gap-1 w-full h-full overflow-y-auto pt-16 mx-auto">
