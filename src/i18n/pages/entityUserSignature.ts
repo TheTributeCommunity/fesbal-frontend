16060d93bac1692ccc4736c0734f5fc82d4472aa
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-03-14 12:46:45 +0000

    Add popup text to i18n for entityUserSignature

--- src/i18n/pages/entityUserSignature.ts
+++ src/i18n/pages/entityUserSignature.ts
@@ -6,6 +6,16 @@ export const entityUserSignatureEs = {
     rejectFoodList: 'Rechazar lista de alimentos',
     swipeToSign: 'Desliza para firmar',
     signed: 'Firmado',
+    sweetAlertSuccess: {
+        title: 'Recogida confirmada',
+        text: '¡Muchas gracias por tu labor! Puedes revisar los detalles de recogida en la pantalla principal.',
+        confirmButtonText: 'Ir a pantalla principal',
+    },
+    sweetAlertReject: {
+        title: 'Lista de alimentos rechazada',
+        text: 'Por favor, revisa el listado de alimentos junto con el beneficiario.',
+        confirmButtonText: 'Revisar lista',
+    }
 }
 
 export const entityUserSignatureEn = {
@@ -16,4 +26,14 @@ export const entityUserSignatureEn = {
     rejectFoodList: 'Reject food list',
     swipeToSign: 'Swipe to sign',
     signed: 'Signed',
+    sweetAlertSuccess: {
+        title: 'Collection confirmed',
+        text: 'Thank you very much for your work! You can check the collection details on the main screen.',
+        confirmButtonText: 'Go to main screen',
+    },
+    sweetAlertReject: {
+        title: 'Food list rejected',
+        text: 'Please, check the food list with the beneficiary.',
+        confirmButtonText: 'Check list',
+    }
 }
