ca75f0f553591f570a07bc76de68c955610d956b
Samuel Domínguez Lorenzo <samueldominguez@protonmail.com>
2023-04-12 17:01:51 +0100

    roboto flex now actually used

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,6 +18,7 @@ module.exports = {
                 'ghost-white': '#F9FDFF',
             },
             fontFamily: {
+                'sans': ['Roboto Flex', 'Arial'],
                 'roboto-flex': ['Roboto Flex', 'Arial']
             },
             fontSize: {
