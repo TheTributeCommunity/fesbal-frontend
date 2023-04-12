8d8ade380349c2654a37eb5b1593ee16afb0d5a1
Samuel Domínguez Lorenzo <samueldominguez@protonmail.com>
2023-04-12 22:29:08 +0100

    relatives updated with subscriptions

--- src/hooks/useRegisterFamilyMembers.ts
+++ src/hooks/useRegisterFamilyMembers.ts
@@ -4,17 +4,24 @@ import { useTranslation } from 'react-i18next'
 import { namespaces } from '../i18n/i18n.constants'
 import AppPopupAlert from '../components/atom/AppPopupAlert'
 import { RecipientUser } from '../models/recipient-user'
-import { RecipientUserService } from '../services/recipient-user-service'
+import { RecipientUserService, SUBSCRIBE_TO_RECIPIENT_USER } from '../services/recipient-user-service'
 import { Relative } from '../models/relative'
 import { AppRoute } from '../enums/app-route'
 import { UsersContext } from '../contexts/usersContext'
 import { RelativeService } from '../services/relative-service'
+import { useSubscription } from '@apollo/client'
 
 const useRegisterFamilyMembers = () => {
     const { t: translate } = useTranslation(namespaces.pages.registerFamilyMembers)
     const [familyMembers, setFamilyMembers] = useState<Relative[]>([])
     const [user, setUser] = useState<RecipientUser>()
     const { firebaseUser } = useContext(UsersContext)
+    useSubscription(SUBSCRIBE_TO_RECIPIENT_USER,
+        {
+            variables: { id: user?.id ?? '' },
+            skip: user ? false : true,
+            onSubscriptionData: () => fetchRelatives(user?.id ?? '').then(relatives => setFamilyMembers(relatives))
+        })
 
     useEffect(() => {
         if (firebaseUser) {
@@ -22,12 +29,21 @@ const useRegisterFamilyMembers = () => {
                 .then(recipientUser => {
                     setUser(recipientUser)
                     recipientUser.relativesIds &&
-                        RelativeService.getAllByRecipientUserId(firebaseUser.uid)
+                        fetchRelatives(firebaseUser.uid)
                             .then(relatives => setFamilyMembers(relatives))
                 })
                 .catch(e => console.log(e))
         }
     }, [firebaseUser])
+
+    const fetchRelatives = async (id: string): Promise<Relative[]> => {
+        const relatives = await RelativeService.getAllByRecipientUserId(id)
+            .catch(e => {
+                console.log(e)
+                return []
+            })
+        return relatives
+    }
 
     const navigate = useNavigate()
 
