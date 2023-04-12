b4e6f56c3d669c87241451731b054667f4702f58
Samuel Domínguez Lorenzo <samueldominguez@protonmail.com>
2023-04-12 13:05:05 +0100

    changed spinner

--- src/components/atom/Spinner.tsx
+++ src/components/atom/Spinner.tsx
@@ -1,16 +1,16 @@
-import { ColorRing } from 'react-loader-spinner'
+import { TailSpin } from 'react-loader-spinner'
 
 const Spinner = (): JSX.Element => {
     return (
         <div className="flex w-full h-full justify-center items-center">
-            <ColorRing
+            <TailSpin
                 visible={true}
                 height="80"
                 width="80"
                 ariaLabel="blocks-loading"
                 wrapperStyle={{}}
                 wrapperClass="blocks-wrapper"
-                colors={['#0F95CE', '#002E5D', '#e8f8ff', '#058CCC', '#F9FDFF']}
+                color={'#0F95CE'}
             /></div>
     )
 }
