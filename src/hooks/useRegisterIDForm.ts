b978a374b9db38775aa235a6aa06feace24ead97
Samuel Domínguez Lorenzo <samueldominguez@protonmail.com>
2023-04-17 18:05:38 +0100

    ID optional depending on age, input characters limited

--- src/hooks/useRegisterIDForm.ts
+++ src/hooks/useRegisterIDForm.ts
@@ -1,21 +1,24 @@
-import {FormEvent, useState} from 'react'
-import {useNavigate} from 'react-router-dom'
-import {validateDNI, validateNIE} from '../helpers'
-import {AppRoute} from '../enums/app-route'
+import { FormEvent, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { validateDNI, validateNIE } from '../helpers'
+import { AppRoute } from '../enums/app-route'
+import { getDiffInYears } from '../helpers/dateHelper'
 
 const useRegisterIDForm = () => {
     const [selectedOption, setSelectedOption] = useState<string>('')
     const [userID, setUserID] = useState<string>('')
 
     const navigate = useNavigate()
-    const validateUserID = (): boolean => {
-        return selectedOption === 'DNI' ? validateDNI(userID) : validateNIE(userID)
+    const validateUserID = (birthDate?: Date): boolean => {
+        if (birthDate && getDiffInYears(birthDate, new Date()) < 18) return true
+        else return selectedOption === 'DNI' ? validateDNI(userID) : validateNIE(userID)
     }
     const onSelectedOptionChange = (option: string) => {
         setSelectedOption(option)
     }
     const onUserIDChange = (id: string) => {
-        setUserID(id)
+        // filter everything except letters and numbers
+        setUserID(id.replace(/[^a-zA-Z0-9]/g, ''))
     }
 
     const onSubmit = (e: FormEvent<HTMLFormElement>) => {
