17ae31ff17347c49b345e538185cfa9f516a67c6
Santi <santi.gamo@gmail.com>
2023-05-04 12:07:29 +0200

    update

--- src/mocks/entity.mock.ts
+++ src/mocks/entity.mock.ts
@@ -4,7 +4,6 @@ const entityMock: Entity = {
     name: 'Asociación de Vecinos de Máspalomas',
     code: 'LPR123',
     province: 'Las Palmas de Gran Canaria',
-    nextDelivery: '12/07/2022',
     address: 'Calle de los Jazmines 8, 35100 San Bartolomé de Tirajana, Las Palmas',
     supervisorName: 'Juan Pérez',
     contactPerson: 'Emilio de la Cruz Vázquez',
