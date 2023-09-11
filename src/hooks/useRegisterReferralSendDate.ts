3e971215da27b9c2142b9f0beb38edea2daaadf9
Alexis Guanche <alexisgr194@gmail.com>
2023-09-11 12:57:15 +0100

    Update date format

--- src/hooks/useRegisterReferralSendDate.ts
+++ src/hooks/useRegisterReferralSendDate.ts
@@ -6,47 +6,50 @@ import { RegistrationRequestService } from '../services/registration-request-ser
 import { useUserStore } from '../store/logged-user'
 
 const useRegisterReferralSendDate = () => {
-    const [selectedDate, setSelectedDate] = useState<Date>()
-    const userId = useUserStore(state => state.userId)
+  const [selectedDate, setSelectedDate] = useState<Date>()
+  const userId = useUserStore((state) => state.userId)
 
-    const isValidFutureDate = () => selectedDate ? selectedDate > new Date() : false
+  const isValidFutureDate = () =>
+    selectedDate ? selectedDate > new Date() : false
 
-    const getFormattedDate = (): string => {
-        if (selectedDate) {
-            const day = selectedDate.getDate()
-            const month = selectedDate.getMonth()
-            const year = selectedDate.getFullYear()
-            const dayPad = day < 10 ? '0' : ''
-            const monthPad = month < 10 ? '0' : ''
-            return `${dayPad}${day}/${monthPad}${month}/${year}`
-        } else return ''
-    }
+  const getFormattedDate = (): string => {
+    if (selectedDate) {
+      const day = selectedDate.getDate()
+      const month = selectedDate.getMonth()
+      const year = selectedDate.getFullYear()
+      const dayPad = day < 10 ? '0' : ''
+      const monthPad = month < 10 ? '0' : ''
+      return `${dayPad}${day}/${monthPad}${month}/${year}`
+    } else return ''
+  }
 
-    const setDate = (dateString: string) => { setSelectedDate(new Date(dateString)) }
+  const setDate = (dateString: string) => {
+    setSelectedDate(new Date(dateString))
+  }
 
-    const submitRegistrationRequest = async (): Promise<boolean> => {
-        if (userId) {
-            const payload: RegistrationRequest = {
-                registrationRequestId: uuidv4(),
-                socialServiceAppointment: selectedDate?.toISOString()
-            }
-            const result = await RegistrationRequestService.send(payload)
-                .then(result => result)
-                .catch(e => {
-                    console.log(e)
-                    return false
-                })
-            return result
-        } else return false
-    }
+  const submitRegistrationRequest = async (): Promise<boolean> => {
+    if (userId) {
+      const payload: RegistrationRequest = {
+        registrationRequestId: uuidv4(),
+        socialServiceAppointment: selectedDate?.getTime(),
+      }
+      const result = await RegistrationRequestService.send(payload)
+        .then((result) => result)
+        .catch((e) => {
+          console.log(e)
+          return false
+        })
+      return result
+    } else return false
+  }
 
-    return {
-        selectedDate,
-        setDate,
-        isValidFutureDate,
-        getFormattedDate,
-        submitRegistrationRequest
-    }
+  return {
+    selectedDate,
+    setDate,
+    isValidFutureDate,
+    getFormattedDate,
+    submitRegistrationRequest,
+  }
 }
 
 export default useRegisterReferralSendDate
